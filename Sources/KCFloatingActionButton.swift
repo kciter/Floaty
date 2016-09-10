b7da1afe045ad1f27099353ba7ad4ce6e8229137
Lee Sun-Hyoup <kciter@naver.com>
2016-09-10 20:39:47 +0900

    Fixed changing title issue

--- Sources/KCFloatingActionButton.swift
+++ Sources/KCFloatingActionButton.swift
@@ -259,7 +259,7 @@ public class KCFloatingActionButton: UIView {
             setOverlayView()
             self.superview?.insertSubview(overlayView, aboveSubview: self)
             self.superview?.bringSubviewToFront(self)
-            overlayView.addTarget(self, action: Selector("close"), forControlEvents: UIControlEvents.TouchUpInside)
+            overlayView.addTarget(self, action: #selector(KCFloatingActionButton.close), forControlEvents: UIControlEvents.TouchUpInside)
             
             UIView.animateWithDuration(0.3, delay: 0,
                 usingSpringWithDamping: 0.55,
@@ -294,7 +294,7 @@ public class KCFloatingActionButton: UIView {
     */
     public func close() {
         if(items.count > 0){
-            self.overlayView.removeTarget(self, action: Selector("close"), forControlEvents: UIControlEvents.TouchUpInside)
+            self.overlayView.removeTarget(self, action: #selector(KCFloatingActionButton.close), forControlEvents: UIControlEvents.TouchUpInside)
             UIView.animateWithDuration(0.3, delay: 0,
                 usingSpringWithDamping: 0.6,
                 initialSpringVelocity: 0.8,
@@ -587,9 +587,9 @@ public class KCFloatingActionButton: UIView {
     }
     
     private func setObserver() {
-        NSNotificationCenter.defaultCenter().addObserver(self, selector: "deviceOrientationDidChange:", name: UIDeviceOrientationDidChangeNotification, object: nil)
-        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillShow:", name:UIKeyboardWillShowNotification, object: nil)
-        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillHide:", name:UIKeyboardWillHideNotification, object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(KCFloatingActionButton.deviceOrientationDidChange(_:)), name: UIDeviceOrientationDidChangeNotification, object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(KCFloatingActionButton.keyboardWillShow(_:)), name:UIKeyboardWillShowNotification, object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(KCFloatingActionButton.keyboardWillHide(_:)), name:UIKeyboardWillHideNotification, object: nil)
     }
     
     deinit {
