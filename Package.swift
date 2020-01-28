7cbf7f1c85765261eac569f59c40fa6cd23e7f9a
Oleksandr Zarochintsev <alex.zarochintsev@gmail.com>
2020-01-28 19:22:11 +0200

    Update package file.

--- Package.swift
+++ Package.swift
@@ -1,14 +1,25 @@
-//
-//  Package.swift
-//  Floaty
-//
-//  Created by Oleksandr Zarochintsev on 28.01.2020.
-//  Copyright © 2019 Oleksandr Zarochintsev. All rights reserved.
-//
+// swift-tools-version:5.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
-let package = Package(name: "Floaty",
-                      platforms: [.iOS(.v10)],
-                      products: [.library(name: "Floaty", targets: ["Floaty"])],
-                      targets: [.target(name: "Floaty", path: "Sources")], swiftLanguageVersions: [.v5])
+let package = Package(
+    name: "Floaty",
+    products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(
+            name: "Floaty",
+            targets: ["Floaty"]),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(
+            name: "Floaty",
+            path: "Sources"),
+    ]
+)
